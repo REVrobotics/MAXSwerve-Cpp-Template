167bdaeb24ffa861225e22c8f78ae221880b1ec5
Jan-Felix Abellera <jfabellera@gmail.com>
2024-01-08 20:01:53 -0600

    Import project to WPILib 2024

--- src/main/cpp/subsystems/DriveSubsystem.cpp
+++ src/main/cpp/subsystems/DriveSubsystem.cpp
@@ -24,14 +24,16 @@ DriveSubsystem::DriveSubsystem()
       m_rearRight{kRearRightDrivingCanId, kRearRightTurningCanId,
                   kRearRightChassisAngularOffset},
       m_odometry{kDriveKinematics,
-                 frc::Rotation2d(units::radian_t{m_gyro.GetAngle()}),
+                 frc::Rotation2d(units::radian_t{
+                     m_gyro.GetAngle(frc::ADIS16470_IMU::IMUAxis::kZ)}),
                  {m_frontLeft.GetPosition(), m_frontRight.GetPosition(),
                   m_rearLeft.GetPosition(), m_rearRight.GetPosition()},
                  frc::Pose2d{}} {}
 
 void DriveSubsystem::Periodic() {
   // Implementation of subsystem periodic method goes here.
-  m_odometry.Update(frc::Rotation2d(units::radian_t{m_gyro.GetAngle()}),
+  m_odometry.Update(frc::Rotation2d(units::radian_t{
+                        m_gyro.GetAngle(frc::ADIS16470_IMU::IMUAxis::kZ)}),
                     {m_frontLeft.GetPosition(), m_rearLeft.GetPosition(),
                      m_frontRight.GetPosition(), m_rearRight.GetPosition()});
 }
@@ -110,7 +112,8 @@ void DriveSubsystem::Drive(units::meters_per_second_t xSpeed,
       fieldRelative
           ? frc::ChassisSpeeds::FromFieldRelativeSpeeds(
                 xSpeedDelivered, ySpeedDelivered, rotDelivered,
-                frc::Rotation2d(units::radian_t{m_gyro.GetAngle()}))
+                frc::Rotation2d(units::radian_t{
+                    m_gyro.GetAngle(frc::ADIS16470_IMU::IMUAxis::kZ)}))
           : frc::ChassisSpeeds{xSpeedDelivered, ySpeedDelivered, rotDelivered});
 
   kDriveKinematics.DesaturateWheelSpeeds(&states, DriveConstants::kMaxSpeed);
@@ -152,12 +155,16 @@ void DriveSubsystem::ResetEncoders() {
 }
 
 units::degree_t DriveSubsystem::GetHeading() const {
-  return frc::Rotation2d(units::radian_t{m_gyro.GetAngle()}).Degrees();
+  return frc::Rotation2d(
+             units::radian_t{m_gyro.GetAngle(frc::ADIS16470_IMU::IMUAxis::kZ)})
+      .Degrees();
 }
 
 void DriveSubsystem::ZeroHeading() { m_gyro.Reset(); }
 
-double DriveSubsystem::GetTurnRate() { return -m_gyro.GetRate().value(); }
+double DriveSubsystem::GetTurnRate() {
+  return -m_gyro.GetRate(frc::ADIS16470_IMU::IMUAxis::kZ).value();
+}
 
 frc::Pose2d DriveSubsystem::GetPose() { return m_odometry.GetPose(); }
 
